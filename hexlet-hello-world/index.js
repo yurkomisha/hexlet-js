9e918bc33f553104d8a4fc1e9e514ef1c8ccd898
Misha Yurko <yurkomisha@gmail.com>
2022-02-03 23:31:21 +0300

    Update index.js

--- hexlet-hello-world/index.js
+++ hexlet-hello-world/index.js
@@ -1,4 +1,12 @@
 // Файл лежит в репозитории github, редактирую его и т.п. в VS Code. С помощью git передаю
 // его на github. Node.js в терминале.
 
-console.log('Hello, Misha!')
+// console.log('Hello, Misha!')
+// console.log(Math.round(5.34))
+
+import fs from "fs";
+import _ from "lodash";
+
+console.log(_.intersection([2, 1], [2, 3]))
+console.log(_.capitalize('hello'))
+console.log(_.last(['one', 'two']))
