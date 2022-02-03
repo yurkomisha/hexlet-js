5e902b689b9a46bfa1725b3b85fa92a851fdbe05
Misha Yurko <yurkomisha@gmail.com>
2022-02-03 23:58:15 +0300

    index

---
+++ index.js
@@ -0,0 +1,6 @@
+// Файл лежит в репозитории github, редактирую его и т.п. в VS Code. С помощью git передаю
+// его на github. Node.js в терминале.
+
+// console.log('Hello, Misha!')
+
+console.log(Math.round(5.34))
