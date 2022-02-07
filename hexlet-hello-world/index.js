25ab52c9d08ac22e36eee8e48fb8e15594b474b3
Misha Yurko <yurkomisha@gmail.com>
2022-02-07 20:54:25 +0300

    Add submodule

--- hexlet-hello-world/index.js
+++ hexlet-hello-world/index.js
@@ -4,16 +4,16 @@
 // console.log('Hello, Misha!')
 // console.log(Math.round(5.34))
 
-import fs from 'fs';
-import _ from 'lodash';
+import fs from 'fs'
+import _ from 'lodash'
 
-console.log(_.intersection([2, 1], [2, 3]));
-console.log(_.capitalize('hello'));
-console.log(_.last(['one', 'two']));
-console.log(_.compact([0, 1, false, 2, '', 3]));
-console.log(_.union([2], [1, 2]));
-console.log(_.zip(['a', 'b'], [1, 2], [true, false]));
+console.log(_.intersection([2, 1], [2, 3]))
+console.log(_.capitalize('hello'))
+console.log(_.last(['one', 'two']))
+console.log(_.compact([0, 1, false, 2, '', 3]))
+console.log(_.union([2], [1, 2]))
+console.log(_.zip(['a', 'b'], [1, 2], [true, false]))
 
-const object = { a: { b: 2 } };
-console.log(_.has(object, ['a', 'b']));
-console.log(_.get(object, 'a.b.c', 'default'));
+const object = { a: { b: 2 } }
+console.log(_.has(object, ['a', 'b']))
+console.log(_.get(object, 'a.b.c', 'default'))
